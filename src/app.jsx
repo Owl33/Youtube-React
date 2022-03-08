030b947bfd9afeb22bb6679c4e0eb4b7458feb38
Geonmin Park <wianw@naver.com>
2022-03-08 17:22:22 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,25 @@
-import './app.css';
+import React, { useEffect, useState } from "react";
+import "./app.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return <h1>hello :)</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCe4q1ScShJa8dqpnXmjwkc3uMQ-cJV-gI",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
